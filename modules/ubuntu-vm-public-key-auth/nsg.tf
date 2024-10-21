eefd1870093271f1e2bafb06ee2b44355136477b
kolosovpetro <kolosovp94@gmail.com>
2024-10-21 21:16:25 +0200

    work in progress

--- modules/ubuntu-vm-public-key-auth/nsg.tf
+++ modules/ubuntu-vm-public-key-auth/nsg.tf
@@ -60,6 +60,20 @@ resource "azurerm_network_security_rule" "allow_prom_scrape_port" {
   network_security_group_name = azurerm_network_security_group.public.name
 }
 
+resource "azurerm_network_security_rule" "allow_prom_server_dashboard_port" {
+  name                        = "AllowPromServerDashboardPort"
+  priority                    = 1040
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "9090"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.public.name
+}
+
 resource "azurerm_network_interface_security_group_association" "public" {
   network_interface_id      = azurerm_network_interface.public.id
   network_security_group_id = azurerm_network_security_group.public.id
