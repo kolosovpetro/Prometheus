85c03f2fa30010f21800e75e290d51464a708f14
kolosovpetro <kolosovp94@gmail.com>
2025-01-31 23:55:08 +0100

    automate cloudflare dns with pwsh

--- output.tf
+++ output.tf
@@ -1,23 +1,11 @@
-output "prometheus_server_data" {
-  value = {
-    ip_address = module.prometheus_master_node_linux.public_ip_address
-    fqdn       = "${local.prometheus_master}.razumovsky.me"
-    http_link  = "http://${local.prometheus_master}.razumovsky.me"
-  }
+output "prometheus_master_public_ip" {
+  value = module.prometheus_master_node_linux.public_ip_address
 }
 
-output "linux_target_data" {
-  value = {
-    ip_address = module.target_node_linux.public_ip_address
-    fqdn       = "${local.linux_target}.razumovsky.me"
-    http_link  = "http://${local.linux_target}.razumovsky.me"
-  }
+output "linux_target_public_ip" {
+  value = module.target_node_linux.public_ip_address
 }
 
-output "windows_target_data" {
-  value = {
-    ip_address = module.target_node_windows.public_ip_address
-    fqdn       = "${local.windows_target}.razumovsky.me"
-    http_link  = "http://${local.windows_target}.razumovsky.me"
-  }
+output "windows_target_public_ip" {
+  value = module.target_node_windows.public_ip_address
 }
