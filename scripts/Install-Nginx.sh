5eb1534d2e32754c010da070ad28e57dd26625e0
kolosovpetro <kolosovp94@gmail.com>
2025-02-01 19:38:48 +0100

    update scripts

--- scripts/Install-Nginx.sh
+++ scripts/Install-Nginx.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-echo "Adding repositories"
-
-sudo DEBIAN_FRONTEND=noninteractive add-apt-repository main -y
-sudo DEBIAN_FRONTEND=noninteractive add-apt-repository universe -y
-sudo DEBIAN_FRONTEND=noninteractive add-apt-repository restricted -y
-sudo DEBIAN_FRONTEND=noninteractive add-apt-repository multiverse -y
-
-echo "Get update package list"
-
-sudo apt-get update -y
-
 echo "Install nginx and build essential"
 
 sudo apt-get install -y build-essential
