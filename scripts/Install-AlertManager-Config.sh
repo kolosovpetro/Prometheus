5d4f9aaf1ac178e84f3d64c611ce7a86d70aa901
kolosovpetro <kolosovp94@gmail.com>
2025-03-01 23:07:55 +0100

    alert manager config

---
+++ scripts/Install-AlertManager-Config.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+
+CONFIG_URL="https://raw.githubusercontent.com/kolosovpetro/Prometheus/refs/heads/AZ400-330/prometheus/alertmanager.yml"  # Change this URL to your actual file
+CONFIG_PATH="/etc/alertmanager/alertmanager.yml"
+
+sudo mkdir -p /etc/alertmanager
+sudo wget -O "$CONFIG_PATH" "$CONFIG_URL"
