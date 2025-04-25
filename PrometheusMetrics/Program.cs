8bb36ab915b727ca0ee1f6ca9940f29708323a31
kolosovpetro <kolosovp94@gmail.com>
2025-04-25 22:58:04 +0200

    bump net 8 | add otel

--- PrometheusMetrics/Program.cs
+++ PrometheusMetrics/Program.cs
@@ -1,32 +1,53 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Prometheus;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var serviceName = "DemoApi";
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+// Configure OpenTelemetry Tracing
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation() // Trace incoming HTTP requests
+        .AddHttpClientInstrumentation() // Trace outgoing HTTP requests
+        .AddConsoleExporter())          // Export traces to the console
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddPrometheusExporter()        // Add exporter: dotnet add package --prerelease OpenTelemetry.Exporter.Prometheus.AspNetCore
+        .AddConsoleExporter());
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
+        .AddConsoleExporter();
+});
+
+// Configure Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-// Exposes metrics under /metrics: https://localhost:7185/metrics
-app.UseMetricServer();
-app.UseHttpMetrics();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapPrometheusScrapingEndpoint();
 
 app.Run();
-
-app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
