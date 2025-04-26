4b2db33df042063f79dbb9884f4a259b1a866913
kolosovpetro <kolosovp94@gmail.com>
2025-04-26 10:44:05 +0200

    pipelines update

--- PrometheusMetrics/Program.cs
+++ PrometheusMetrics/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
