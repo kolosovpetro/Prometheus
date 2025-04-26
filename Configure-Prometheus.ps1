c183485379473c0b035825ed21c77fd3b5f29bbf
kolosovpetro <kolosovp94@gmail.com>
2025-04-26 11:09:46 +0200

    update config script

--- Configure-Prometheus.ps1
+++ Configure-Prometheus.ps1
@@ -4,12 +4,12 @@ $ErrorActionPreference = "Stop"
 
 $masterPublicIp = $( terraform output -raw "master_public_ip" )
 
-scp ./prometheus/prometheus.yml razumovsky_r@$($masterPublicIp):/tmp/prometheus
-ssh razumovsky_r@$($masterPublicIp) "sudo mv /tmp/prometheus /etc/prometheus"
+scp -o StrictHostKeyChecking=no ./prometheus/prometheus.yml razumovsky_r@$($masterPublicIp):/tmp/prometheus
+ssh -o StrictHostKeyChecking=no razumovsky_r@$($masterPublicIp) "sudo mv /tmp/prometheus /etc/prometheus"
 
 $restartCommand = "sudo systemctl daemon-reload && sudo systemctl restart prometheus.service && sudo systemctl status prometheus.service --no-pager"
 
-ssh razumovsky_r@$($masterPublicIp) $restartCommand
+ssh -o StrictHostKeyChecking=no razumovsky_r@$($masterPublicIp) $restartCommand
 
 Write-Host "Prometheus configured successfully!" -ForegroundColor Green
 
